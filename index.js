fe3fed737cd41bdde10abc7730c414dea2255a24
Monica Seaman <monica.l.seaman@gmail.com>
2018-03-30 15:37:49 +0000

    Done.

--- index.js
+++ index.js
@@ -14,24 +14,12 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  let childs = document.getElementById('grand-node').querySelectorAll('div');
-  let smallerKids = [];
-  while(childs !== undefined) {
-    if(childs.length === 1) {
-      var onlyChild = childs.children;
-      if(onlyChild !== undefined) {
-        childs = onlyChild;
-      } else {
-        return childs;
-      }
-    } else {
-      for(var i=0; i<childs.length; i++) {
-        if (childs[i].children !== undefined) {
-          smallerKids.push(childs[i].children);
-        }  
-      }
-      childs = smallerKids;
-    }
+  let parentNode = document.getElementById('grand-node');
+  var childNode = parentNode.children[0];
+  while(childNode) {
+    parentNode = childNode;
+    childNode = parentNode.children[0];
   }
+  return parentNode;
 }
 

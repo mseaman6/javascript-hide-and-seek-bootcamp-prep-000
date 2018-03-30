8e7ec6476c970879543297d8a8a18eceb05edc25
Monica Seaman <monica.l.seaman@gmail.com>
2018-03-30 14:50:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,22 +16,24 @@ function increaseRankBy(n) {
 function deepestChild() {
   let childs = document.getElementById('grand-node').querySelectorAll('div');
   let smallerKids = [];
-  while(childs !== undefined) {
-    if(childs.length === 1) {
-      var onlyChild = childs.children;
-      if(onlyChild !== undefined) {
-        childs = onlyChild;
-      } else {
-        return childs;
-      }
-    } else {
-      for(var i=0; i<childs.length; i++) {
-        if (childs[i].children !== undefined) {
-          smallerKids.push(childs[i].children);
-        }  
-      }
-      childs = smallerKids;
-    }
-  }
+  debugger;
+  // while(childs !== undefined) {
+  //   if(childs.length === 1) {
+  //     var onlyChild = childs.children;
+  //     if(onlyChild !== undefined) {
+  //       childs = onlyChild;
+  //     } else {
+  //       return childs;
+  //     }
+  //   } else {
+  //     for(var i=0; i<childs.length; i++) {
+  //       if (childs[i].children !== undefined) {
+  //         smallerKids.push(childs[i].children);
+  //       }  
+  //     }
+  //     childs = smallerKids;
+  //   }
+  // }
 }
+
 
